35d5e6a25b9d25db862847f8c736f2ca451a729a
HariNayan <dev.harinayan@gmail.com>
2026-05-12 01:48:31 +0530

    made Conversational Multi-PDF chat and initi StreamlitUi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+from llm import create_llm
 
 st.set_page_config(
     page_title = 'Multi-PDF Chatbot',
@@ -25,3 +26,21 @@ if 'history' not in st.session_state:
 
 if 'messages' not in st.session_state:
     st.session_state.messages = []
+
+for messages in st.session_state.messages:
+    with st.chat_message(messages['role']):
+        st.markdown(messages['content'])
+        
+
+if prompt:= st.chat_input("Enter Query Here"):
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    st.session_state.messages.append({'role': 'user' , 'content': prompt})
+    answer = create_llm(prompt, st.session_state.history)
+
+    with st.chat_message('assistant'):
+        st.markdown(answer)
+    
+    st.session_state.messages.append({'role': 'assistant', 'content': answer})
+    st.session_state.history.append(f"User: {prompt}")
+    st.session_state.history.append(f"Assistant: {answer}")
